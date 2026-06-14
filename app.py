d236420c523892b01bfaf6eee8241e1ac30172df
prasanth104069-dot <prasanth104069@gmail.com>
2026-06-14 11:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey123'
@@ -11,13 +11,26 @@ def login():
         password = request.form.get('password')
 
         if staff_id == 'admin' and password == 'admin123':
-            flash('Login successful!', 'success')
-            return redirect(url_for('login'))
+            session['staff_name'] = 'Admin'
+            return redirect(url_for('dashboard'))
         else:
             flash('Invalid credentials. Try again.', 'error')
             return redirect(url_for('login'))
 
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'staff_name' not in session:
+        return redirect(url_for('login'))
+    return render_template('dashboard.html',
+                           staff_name=session['staff_name'],
+                           total_students=3)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
