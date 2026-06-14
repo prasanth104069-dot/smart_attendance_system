5e6d156b03ef2f12f7b93036d089487739b846f9
prasanth104069-dot <prasanth104069@gmail.com>
2026-06-14 21:00:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+app.secret_key = 'mysecretkey123'
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        staff_id = request.form.get('staff_id')
+        password = request.form.get('password')
+
+        if staff_id == 'admin' and password == 'admin123':
+            session['staff_name'] = 'Admin'
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid credentials. Try again.', 'error')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'staff_name' not in session:
+        return redirect(url_for('login'))
+    return render_template('dashboard.html',
+                           staff_name=session['staff_name'],
+                           total_students=3)
+
+@app.route('/mark-attendance')
+def mark_attendance():
+    if 'staff_name' not in session:
+        return redirect(url_for('login'))
+    return render_template('attendance.html')
+                               
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
